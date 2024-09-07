e28c4712bf01f298d0b45dc3285d663d5902f54f
Oleksandr Agniev <70901180+agniev-a-hub@users.noreply.github.com>
2024-09-07 16:55:29 +0200

    20240907165031-bootstrap (#1)

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { envSchema } from './env/env';
+import { EnvModule } from './env/env.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
