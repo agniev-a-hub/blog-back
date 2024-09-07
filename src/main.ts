23fa756aacfda42e0963c84d28089661344b28df
Oleksandr Agniev <oleksandr.agniev@diversido.io>
2024-09-07 16:54:04 +0200

    20240907165031-bootstrap

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata"
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { EnvService } from './env/env.service';
+
+async function start() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(EnvService);
+  const port = configService.get('PORT');
+  await app.listen(port);
+}
+
+start();
