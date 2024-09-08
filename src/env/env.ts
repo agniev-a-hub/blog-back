b564095086bdfd3a9fa8408064acb422c976533e
Oleksandr Agniev <70901180+agniev-a-hub@users.noreply.github.com>
2024-09-08 17:33:27 +0200

    20240908172941 setup first resources (#2)
    * 20240908172941-setup-first-resources
    
    * lefthook-update-due-to-tests-absence

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,8 +1,12 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
-  NODE_ENV: z.coerce.string(),
   PORT: z.coerce.number(),
+  POSTGRES_HOST: z.coerce.string(),
+  POSTGRES_USER: z.coerce.string(),
+  POSTGRES_DB: z.coerce.string(),
+  POSTGRES_PASSWORD: z.coerce.string(),
+  POSTGRES_PORT: z.coerce.number(),
 });
 
 export type Env = z.infer<typeof envSchema>;
